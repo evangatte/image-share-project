e817f15ac1f82e10476c246c21b49215b5891d30
Evan Gatte <evangatte2255@gmail.com>
2022-05-07 17:42:07 -0500

    create admin panel preview grid

--- client/js/photoPanel.js
+++ client/js/photoPanel.js
@@ -1,15 +1,50 @@
-
-// $('#photoDropdown li').click(function() {
-// 	$('#previewImage').attr('src', `/${$(this).text()})`;
-// })
-
+//preview image
 const $previewImage = $('#previewImage');
-
 $('#photoDropdown li').hover(
 	function() {
 	$previewImage.attr('src', `/${$(this).text()}`);
-},
-	function () {
-		$previewImage.attr('src', '')
+})
+$('#photoDropdown li').click(function() {
+	$previewImage.attr('src', `/${$(this).text()}`);	
+}) 
+
+
+
+
+$(document).on('click', function(evt) {
+	if ($(evt.target).hasClass('rowImage')) {
+		$('.previewRow > div').css('border-color', 'black')
+		$('.previewRow > div').removeClass('selected')
+		$(evt.target).css('border-color', 'red');
+		$(evt.target).addClass('selected');
+		
+	}  else if ($(evt.target).parent().hasClass('rowImage')) {
+		$('.previewRow > div').css('border-color', 'black')
+		$('.previewRow > div').removeClass('selected')
+		$(evt.target).parent().css('border-color', 'red');
+		$(evt.target).parent().addClass('selected')
 	}
-)
+});	
+
+
+
+$('#addToGridBtn').click(function() {
+	const $gridSquare = $('.selected > img')	
+	const previewImageSrc = $previewImage.attr('src');
+	console.log(previewImageSrc)
+	
+	$gridSquare.attr('src', `${previewImageSrc}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
